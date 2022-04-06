4efdb85618a7a59c60993e14353d18be9b84f161
mznmk <mika_develop@mznmk.com>
2022-04-06 20:27:53 +0900

    [ex00] change format tab->space (megaphone.cpp)

--- ex00/source/megaphone.cpp
+++ ex00/source/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mmizuno <mmizuno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 16:53:01 by mmizuno           #+#    #+#             */
-/*   Updated: 2021/07/25 17:40:25 by mmizuno          ###   ########.fr       */
+/*   Updated: 2022/04/06 20:27:45 by mmizuno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc <= 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	}
-	else {
-		for (int i = 1; i < argc; i++) {
-			std::string str = argv[i];
-			for (size_t j = 0; j < str.length(); j++) {
-				std::cout << (char)std::toupper(str[j]);
-			}
-		}
-	}
-	std::cout << std::endl;
-	return (0);
+  if (argc <= 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      std::string str = argv[i];
+      for (size_t j = 0; j < str.length(); j++) {
+        std::cout << (char)std::toupper(str[j]);
+      }
+    }
+  }
+  std::cout << std::endl;
+  return (0);
 }
