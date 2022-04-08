bf6272b547ae69584bb506129143ffd39ceb04bd
mznmk <mika_develop@mznmk.com>
2022-04-08 23:22:22 +0900

    [ex00] change tab size megaphone.cpp

--- ex00/source/megaphone.cpp
+++ ex00/source/megaphone.cpp
@@ -6,26 +6,26 @@
 /*   By: mmizuno <mmizuno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 16:53:01 by mmizuno           #+#    #+#             */
-/*   Updated: 2022/04/06 20:27:45 by mmizuno          ###   ########.fr       */
+/*   Updated: 2022/04/08 23:21:51 by mmizuno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-  if (argc <= 1) {
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-  }
-  else {
-    for (int i = 1; i < argc; i++) {
-      std::string str = argv[i];
-      for (size_t j = 0; j < str.length(); j++) {
-        std::cout << (char)std::toupper(str[j]);
-      }
+    if (argc <= 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
-  }
-  std::cout << std::endl;
-  return (0);
+    else {
+        for (int i = 1; i < argc; i++) {
+            std::string str = argv[i];
+            for (size_t j = 0; j < str.length(); j++) {
+                std::cout << (char)std::toupper(str[j]);
+            }
+        }
+    }
+    std::cout << std::endl;
+    return (0);
 }
